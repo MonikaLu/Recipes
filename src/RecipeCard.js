ae41b054089c97b1e75b9f8d3f45f2b4fe986812
Monika Luu <monika-luu@hotmail.com>
2022-12-30 23:54:48 +0100

    init repo

---
+++ src/RecipeCard.js
@@ -0,0 +1,9 @@
+function RecipeCard({recipeTitle, recipeDetails}) {
+
+    return <div>
+        <h2>{recipeTitle}</h2>
+        <div>{recipeDetails}</div>
+        </div>
+}
+
+export default RecipeCard;
