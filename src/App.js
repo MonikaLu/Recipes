f79d5a890d3aa22d94b7a89b9a89f5b99506d40e
Monika Luu <monika-luu@hotmail.com>
2022-12-31 13:48:38 +0100

    :sparkles: Add recipe cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import RecipeCard from "./RecipeCard";
+import TacoImage from "./images/taco.jpeg";
+import KebabImage from "./images/kebab.webp";
+import FajitasImage from "./images/fajitas.jpeg";
+import Masonry from "@mui/lab/Masonry";
 
-function App() {return (
-<div>
-    <div><h1>Recipes</h1></div>
-    <RecipeCard recipeTitle = "Taco" recipeDetails="TacoDetails"/>
-    <RecipeCard recipeTitle = "Kebab" recipeDetails="KebabDetails"/>
-    <RecipeCard recipeTitle = "Fajitas" recipeDetails="FajitasDetails"/>
-</div>
- )
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>Recipes</h1>
+      </div>
+
+      <Masonry columns={3} spacing={2}>
+        <RecipeCard
+          recipeTitle="Taco"
+          recipeDetails="TacoDetails"
+          recipeImage={TacoImage}
+        />
+
+        <RecipeCard
+          recipeTitle="Kebab"
+          recipeDetails="KebabDetails"
+          recipeImage={KebabImage}
+        />
+        <RecipeCard
+          recipeTitle="Fajitas"
+          recipeDetails="FajitasDetails"
+          recipeImage={FajitasImage}
+        />
+      </Masonry>
+    </div>
+  );
 }
 
 export default App;
--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,9 +1,36 @@
-function RecipeCard({recipeTitle, recipeDetails}) {
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
 
-    return <div>
-        <h2>{recipeTitle}</h2>
+function RecipeCard({ recipeTitle, recipeDetails, recipeImage }) {
+  return (
+    <div>
+      {/* <h2>{recipeTitle}</h2>
         <div>{recipeDetails}</div>
-        </div>
+        <img src={recipeImage} alt="failed to load"/> */}
+
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia sx={{ height: 200 }} title="Taco" image={recipeImage} />
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {recipeTitle}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {recipeDetails}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
 }
 
 export default RecipeCard;
